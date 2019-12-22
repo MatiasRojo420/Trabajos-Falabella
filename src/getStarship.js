ee1c76ffb926043d48db1cdd2c769941548d5dbd
Matías Rojo <58794860+MatiasRojo420@users.noreply.github.com>
2019-12-22 20:50:29 -0300

    Update getStarship.js

--- src/getStarship.js
+++ src/getStarship.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const bodyParser = require('body-parser')
 
 const getStarships = async (req, res) => {
-    const response = await axios('https://swapi.co/api/starships/')
+    const response = await axios.get('https://swapi.co/api/starships/')
     const { results } = response.data;
 
     return results
