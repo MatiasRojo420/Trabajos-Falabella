764d9c14c198bd1b649242056f46f50b20dbeaab
Matías Rojo <58794860+MatiasRojo420@users.noreply.github.com>
2019-12-22 21:01:37 -0300

    Update getStarship.test.js

--- _test_/unit/getStarship.test.js
+++ _test_/unit/getStarship.test.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 const getStarship = require('../../src/getStarship')
-//jest.mock('axios')
+jest.mock('axios')
 
 describe('getStarship', () => {
     const resp = {
@@ -25,16 +25,67 @@ describe('getStarship', () => {
                     created: '2014-12-10T15:48:00.586000Z',
                     edited: '2014-12-22T17:35:44.431407Z',
                     url: 'https://swapi.co/api/starships/5/'
+                  },
+                  {
+                    name: 'Imperial shuttle',
+                    model: 'Lambda-class T-4a shuttle',
+                    manufacturer: 'Sienar Fleet Systems',
+                    cost_in_credits: '240000',
+                    length: '20',
+                    max_atmosphering_speed: '850',
+                    crew: '6',
+                    passengers: '20',
+                    cargo_capacity: '80000',
+                    consumables: '2 months',
+                    hyperdrive_rating: '1.0',
+                    MGLT: '50',
+                    starship_class: 'Armed government transport',
+                    pilots: [
+                      'https://swapi.co/api/people/1/',
+                      'https://swapi.co/api/people/13/',
+                      'https://swapi.co/api/people/14/'
+                    ],
+                    films: [
+                      'https://swapi.co/api/films/2/',
+                      'https://swapi.co/api/films/3/'
+                    ],
+                    created: '2014-12-15T13:04:47.235000Z',
+                    edited: '2014-12-22T17:35:44.795405Z',
+                    url: 'https://swapi.co/api/starships/22/'
+                  },
+                  {
+                    name: 'EF76 Nebulon-B escort frigate',
+                    model: 'EF76 Nebulon-B escort frigate',
+                    manufacturer: 'Kuat Drive Yards',
+                    cost_in_credits: '8500000',
+                    length: '300',
+                    max_atmosphering_speed: '800',
+                    crew: '854',
+                    passengers: '75',
+                    cargo_capacity: '6000000',
+                    consumables: '2 years',
+                    hyperdrive_rating: '2.0',
+                    MGLT: '40',
+                    starship_class: 'Escort ship',
+                    pilots: [],
+                    films: [
+                      'https://swapi.co/api/films/2/',
+                      'https://swapi.co/api/films/3/'
+                    ],
+                    created: '2014-12-15T13:06:30.813000Z',
+                    edited: '2014-12-22T17:35:44.848329Z',
+                    url: 'https://swapi.co/api/starships/23/'
                   }
             ]
         }
         
     };
-    //axios.get.mockResolvedValue(resp)
+    axios.get.mockResolvedValue(resp)
     it('should return a list of starships', async() => {
         const result = await getStarship()
         expect(Array.isArray(result)).toBeTruthy
-        //expected(result.length.toEqual(3))
+        expect(result).toHaveLength(3)
+        expect(axios.get).toHaveBeenCalledTimes(1)
         
     })
 })
